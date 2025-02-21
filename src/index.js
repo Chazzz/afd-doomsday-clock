fb78b059380644c83108d73ce120f1935922f9a5
Chazzz <270857+Chazzz@users.noreply.github.com>
2025-02-21 10:15:55 -0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AfDDoomsdayClock from "./AfDDoomsdayClock";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AfDDoomsdayClock />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
